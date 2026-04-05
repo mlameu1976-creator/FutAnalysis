05f0ac8e0135eab789135c9b51802f72e6e76f64
MOACIR <mlameu.1976@gmail.com>
2026-04-05 18:22:08 -0300

    fix: correct postgres connection using pool.query

--- backend/src/db.js
+++ backend/src/db.js
@@ -1,11 +1,10 @@
-import pkg from "pg";
-const { Pool } = pkg;
+const { Pool } = require("pg");
 
 const pool = new Pool({
   connectionString: process.env.DATABASE_URL,
   ssl: {
-    rejectUnauthorized: false,
-  },
+    rejectUnauthorized: false
+  }
 });
 
-export default pool;
+module.exports = pool;
